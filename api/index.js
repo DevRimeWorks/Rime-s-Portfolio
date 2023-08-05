a8bd40da6c9f22814501970182bafb58e0a5e16c
DevRimeWorks <devrimeworks@gmail.com>
2023-08-05 18:27:23 +0300

    sssss

--- api/index.js
+++ api/index.js
@@ -11,10 +11,6 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
 app.post("/", (req, res) => {
     console.log(req.body);
 
